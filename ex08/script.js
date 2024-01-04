ae628d08985bdfb7e6539153e44651c3e5411690
paloma <palomafreire@gmail.com>
2024-01-04 18:10:18 -0300

    criei o html do ex08

---
+++ ex08/script.js
@@ -0,0 +1,6 @@
+function calcular() {
+    let num = window.prompt('Digite um número:');
+    var res = document.getElementById('resultado');
+    res.innerHTML = `O número a ser analisado aqui será o <strong>${num}</strong>`;
+    
+}
