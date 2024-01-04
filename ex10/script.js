358f7f5374f4676347be0373d105f858b8264959
paloma <palomafreire@gmail.com>
2024-01-04 19:45:53 -0300

    finalizei o script do ex10

---
+++ ex10/script.js
@@ -0,0 +1,19 @@
+function acao1() {
+    let res = document.getElementById('resultado');
+    res.innerHTML += `<p>Clicou no primeiro botão.</p>`
+}
+
+function acao2() {
+    let res = document.getElementById('resultado');
+    res.innerHTML += `<p>Clicou no segundo botão.</p>`
+}
+
+function acao3() {
+    let res = document.getElementById('resultado');
+    res.innerHTML += `<p>Clicou no terceiro botão.</p>`
+}
+
+function acao4() {
+    let res = document.getElementById('resultado');
+    res.innerHTML += `<p>Clicou no quarto botão.</p>`
+}
