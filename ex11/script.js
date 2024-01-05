270fed4ba774abe37f0e26f7813062f7ab7e34d9
paloma <palomafreire@gmail.com>
2024-01-05 16:49:44 -0300

    corrigi os acentos

--- ex11/script.js
+++ ex11/script.js
@@ -1,13 +1,13 @@
 function calcularMedia() {
-    let nome = window.prompt(`Qual e o nome do(a) aluno(a)?`);
+    let nome = window.prompt(`Qual é o nome do(a) aluno(a)?`);
     let nota1 = Number(window.prompt(`Qual foi a primeira nota de ${nome}?`));
-    let nota2 = Number(window.prompt(`Alem de ${nota1}, qual foi a outra nota de ${nome}?`));
+    let nota2 = Number(window.prompt(`Além de ${nota1}, qual foi a outra nota de ${nome}?`));
     let media = (nota1 + nota2) / 2;
     let res = document.getElementById('resultado');
-    res.innerHTML = `Calculando a media final de <mark>${nome}</mark>. <br><br>As notas obtidas foram <mark>${nota1}</mark> e <mark>${nota2}</mark>. <br><br>A media final e <mark>${media}</mark>. <br> <br> A mensagem que temos e: `;
+    res.innerHTML = `Calculando a média final de <mark>${nome}</mark>. <br><br>As notas obtidas foram <mark>${nota1}</mark> e <mark>${nota2}</mark>. <br><br>A média final é <mark>${media}</mark>. <br> <br> A mensagem que temos é: `;
     let msg; 
     if (media >= 6) {
-        msg = `Meus parabens!`
+        msg = `Meus parabéns!`
     } else {
         msg = `Estude um pouco mais!`
     }
