a74dcf07adfc52b6508a73a074fdc915a8ff05c4
paloma <palomafreire@gmail.com>
2024-01-05 17:41:13 -0300

    adicionei o teste de valores iguais no ex13

--- ex13/script.js
+++ ex13/script.js
@@ -4,7 +4,10 @@ function maiorValor() {
     let res = document.getElementById('resultado');
     if (num1 > num2) {
         res.innerHTML = `Analisando os valores ${num1} e ${num2}, o maior valor é ${num1}!`;
-    } else {
+    } else if (num2 > num1) {
         res.innerHTML = `Analisando os valores ${num1} e ${num2}, o maior valor é ${num2}!`;
+    } else {
+        res.innerHTML = `Analisando os valores ${num1} e ${num2}, ambos são <mark>IGUAIS</mark>!`;
     }
+
 }
