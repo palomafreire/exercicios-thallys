a6c4c162e4dab46f7b3918297a933f7e38a7b911
paloma <palomafreire@gmail.com>
2024-01-05 16:28:05 -0300

    finalizei o script do ex11

--- ex11/script.js
+++ ex11/script.js
@@ -1,3 +1,14 @@
 function calcularMedia() {
-    
+    let nome = window.prompt(`Qual e o nome do(a) aluno(a)?`);
+    let nota1 = Number(window.prompt(`Qual foi a primeira nota de ${nome}?`));
+    let nota2 = Number(window.prompt(`Alem de ${nota1}, qual foi a outra nota de ${nome}?`));
+    let media = (nota1 + nota2) / 2;
+    let res = document.getElementById('resultado');
+    res.innerHTML = `Calculando a media final de <mark>${nome}</mark>. <br><br>As notas obtidas foram <mark>${nota1}</mark> e <mark>${nota2}</mark>. <br><br>A media final e <mark>${media}</mark>.`;
+    if (media >= 6) {
+        res.innerHTML += `<p> A mensagem que temos e: Meus parabens!</p>`
+    } else {
+        res.innerHTML += `<p> A mensagem que temos e: Estude um pouco mais!</p>`
+    }
+   
 }
