75883dcf5ec09359b23378a7fc61c47ac84ee4e9
paloma <palomafreire@gmail.com>
2024-01-06 11:19:24 -0300

    alterei a visibilidade do botao qdo o jogo termina no ex18

--- ex18/script.js
+++ ex18/script.js
@@ -4,11 +4,10 @@ function jogar() {
     let tentativa = Number(window.prompt('Qual seu palpite?'));
     if (tentativa == alvo) {
         res.innerHTML += `<p>PARABÉNS! Você acertou! Eu tinha sorteado o número <mark>${alvo}</mark>!`;
-    } else {
-        if (tentativa < alvo) {
+        document.getElementById('botao').style.visibility = 'hidden';
+    } else if (tentativa < alvo) {
             res.innerHTML += `<p> Você falou ${tentativa}. Meu número é MAIOR!`;
-        } else {
+            } else {
             res.innerHTML += `<p>Você falou ${tentativa}. Meu número é MENOR!`;
-        }
-    }
+            }
 }
