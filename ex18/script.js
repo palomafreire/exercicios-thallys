7b8912587b87035919e89c484a5d72cd9ab31a5e
paloma <palomafreire@gmail.com>
2024-01-06 11:05:40 -0300

    finalizei a função jogar do ex18

--- ex18/script.js
+++ ex18/script.js
@@ -1,3 +1,14 @@
+const alvo = Math.trunc((Math.random() * 100) + 1);
+let res = document.getElementById('resultado');
 function jogar() {
-    
+    let tentativa = Number(window.prompt('Qual seu palpite?'));
+    if (tentativa == alvo) {
+        res.innerHTML += `<p>PARABÉNS! Você acertou! Eu tinha sorteado o número <mark>${alvo}</mark>!`;
+    } else {
+        if (tentativa < alvo) {
+            res.innerHTML += `<p> Você falou ${tentativa}. Meu número é MAIOR!`;
+        } else {
+            res.innerHTML += `<p>Você falou ${tentativa}. Meu número é MENOR!`;
+        }
+    }
 }
