564cbdeae1a9bdfecf1c4c44e338e0d6d5e1ac07
paloma <palomafreire@gmail.com>
2024-01-06 00:03:21 -0300

    coloquei o tipo Number no input do ex16

--- ex16/script.js
+++ ex16/script.js
@@ -1,5 +1,5 @@
 function calcularIdade() {
-    let ano = window.prompt('Em que ano você nasceu?');
+    let ano = Number(window.prompt('Em que ano você nasceu?'));
     const anoAtual = new Date().getFullYear();
     let res = document.getElementById('resultado');
     res.innerHTML = `Quem nasceu em ${ano} vai completar <mark>${anoAtual - ano}</mark> anos em ${anoAtual}.`
