41f49c52a398942719709747695db6e520c8930d
paloma <palomafreire@gmail.com>
2024-01-05 23:46:54 -0300

    finalizei o script do ex15

--- ex15/script.js
+++ ex15/script.js
@@ -1,17 +1,18 @@
 function analisar() {
     const data = new Date();
-    const semana = ['Sáb','Dom','Seg', 'Ter', 'Qua', 'Qui', 'Sex'];
+    const mes = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
+    const semana = ['Dom','Seg', 'Ter', 'Qua', 'Qui', 'Sex','Sáb'];
 
     let res = document.getElementById('resultado');
     
 
-    res.innerHTML = `<p>Dia: ${data.getDay()}`;
-    res.innerHTML += `<p>Mês: ${data.getMonth() + 1}`;
+    res.innerHTML = `<p>Dia: ${data.getDate()}`;
+    res.innerHTML += `<p>Mês: ${mes[data.getMonth()]}`;
     res.innerHTML += `<p>Ano: ${data.getFullYear()}`;
-    res.innerHTML += `<p>Dia da Semana: ${semana[data.getUTCDay()]}`;
+    res.innerHTML += `<p>Dia da Semana: ${semana[data.getDay()]}`;
     res.innerHTML += `<p>Hora: ${data.getHours()}`;
-    res.innerHTML += `<p>Minuto: ${data.getMinutes()}`;
-    res.innerHTML += `<p>Segundo: ${data.getSeconds()}</p>`;
+    res.innerHTML += `<p>Minutos: ${data.getMinutes()}`;
+    res.innerHTML += `<p>Segundos: ${data.getSeconds()}</p>`;
 
 }
 
