b8d5553af8ad584756e6839178b3a78106cd9d7b
paloma <palomafreire@gmail.com>
2024-01-06 11:59:01 -0300

    ajustes no código do ex19

--- ex19/script.js
+++ ex19/script.js
@@ -3,30 +3,24 @@ let res = document.getElementById('resultado');
 function iniciar() {
     let num1 = Number(window.prompt('Primeiro valor:'));
     let num2 = Number(window.prompt('Segundo valor:'));
-    let operacao = window.prompt(`Valores informados: ${num1} e ${num2}. 
-O que vamos fazer?
-[1] Somar 
-[2] Subtrair 
-[3] Multiplicar 
-[4] Dividir`);
+    let operacao = window.prompt(`Valores informados: ${num1} e ${num2}. \nO que vamos fazer?\n[1] Somar \n[2] Subtrair \n[3] Multiplicar \n[4] Dividir`);
+    
+    res.innerHTML = `Calculando...`;
+    
     switch (operacao) {
         case '1': {
-            res.innerHTML = `Calculando...`;
             res.innerHTML += `<p>${num1} + ${num2} = ${(num1 + num2)}</p>`;
             break;
         }
         case '2': {
-            res.innerHTML = `Calculando...`;
             res.innerHTML += `<p>${num1} - ${num2} = ${(num1 - num2)}</p>`;
             break;
         }
         case '3': {
-            res.innerHTML = `Calculando...`;
             res.innerHTML += `<p>${num1} * ${num2} = ${(num1 * num2)}</p>`;
             break;
         }
         case '4': {
-            res.innerHTML = `Calculando...`;
             res.innerHTML += `<p>${num1} / ${num2} = ${(num1 / num2).toFixed(3)}</p>`;
             break;
         }
