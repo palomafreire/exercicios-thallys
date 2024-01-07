58777a6ccc221bb1f6e074322f54b610529707b6
paloma <palomafreire@gmail.com>
2024-01-06 23:26:13 -0300

    finalizei o ex21

---
+++ ex21/script.js
@@ -0,0 +1,8 @@
+function contar() {
+    let res = document.getElementById('resultado');
+    res.innerHTML += `<p>Contando de 1 até 10: </p>`
+    for (let i = 1; i <= 10; i++) {
+        res.innerHTML += `${i} &#x1F449 `;
+    }
+    res.innerHTML += `&#x1F3C1`;
+}
