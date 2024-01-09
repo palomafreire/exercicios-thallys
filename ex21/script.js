2c07d15010652e3ea76e1c285ba34c6b88d1ec0a
paloma <palomafreire@gmail.com>
2024-01-08 21:44:23 -0300

    criei o html do ex22

--- ex21/script.js
+++ ex21/script.js
@@ -1,6 +1,7 @@
 function contar() {
     let res = document.getElementById('resultado');
     res.innerHTML += `<p>Contando de 1 até 10: </p>`
+    
     for (let i = 1; i <= 10; i++) {
         res.innerHTML += `${i} &#x1F449 `;
     }
