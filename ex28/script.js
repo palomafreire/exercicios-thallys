2126797385a84f61321d0cae4f4867eb462195bd
paloma <palomafreire@gmail.com>
2024-01-09 22:07:20 -0300

    corrigi o x antes do = no ex28

---
+++ ex28/script.js
@@ -0,0 +1,11 @@
+function fatorial() {
+    let res = document.getElementById('resultado');
+    let num = document.getElementById('num').value;
+    res.innerHTML += `<p>Calculando ${num}!</p>`;
+    let fatorial = 1;
+    for (let i = num; i > 1; i--) {
+        fatorial = fatorial * i;
+        res.innerHTML += `${i} x `;
+    }
+    res.innerHTML += `1 = ${fatorial}`;
+}
